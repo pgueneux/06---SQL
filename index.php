e9137072ef7bc3bbaf824ba23122cf01f89b05a2
pgueneux <philippe.gueneux@gmail.com>
2021-01-14 14:12:22 +0100

    mise à jour

--- index.php
+++ index.php
@@ -1,1 +1,31 @@
-<h1>Test</h1>
+<?php
+require_once('./includes/bdd.inc.php'); 
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <?php
+    require('./templates/head.inc.html');
+    ?>
+</head>
+<body>
+    
+<header>
+    <?php
+require('./templates/header.html')
+    ?>
+</header>
+
+<main>
+</main>
+
+<footer>
+<?php
+require('./templates/footer.html')
+?>
+</footer>
+
+</body>
+</html>
